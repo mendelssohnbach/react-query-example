9ac01d5c32364d1d79b99002b74de2059a002a5b
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-22 14:40:10 +0900

    追加 AppコンポーネントにReactQueryDevtools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+// import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,9 +14,11 @@ export default function App() {
 }
 
 function Example() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
+  const { isLoading, error, data, isFetching } = useQuery('repoData', () =>
     fetch(`https://api.github.com/repos/tannerlinsley/react-query`).then((res) => res.json())
   );
+
+  const [count, setCount] = useState(0);
 
   if (isLoading) return 'Loading...';
 
@@ -21,10 +26,14 @@ function Example() {
 
   return (
     <div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button>{count}</button>
       <h1>{data.name}</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong> <strong>✨ {data.stargazers_count}</strong>{' '}
       <strong>🍴 {data.forks_count}</strong>
+      <div>{isFetching ? 'Updating...' : ''}</div>
+      <ReactQueryDevtools initialIsOpen />
     </div>
   );
 }
